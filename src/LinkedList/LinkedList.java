8a7e8ed530cff804215791e2b0f4887f35731b99
Mihail-Valeriu PAVEL <mpavel@P5RO-PORT1490.local>
2024-01-22 16:26:48 +0200

    ll methods added

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -2,7 +2,29 @@ package LinkedList;
 
 public class LinkedList {
 
-    public LinkedList(int value)
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        // point the head to the new Node (also the only one)
+        head = newNode;
+        // point the tail to the new Node (also the only one)
+        tail = newNode;
+        // make the length of the List 1, because we added one element
+        length = 1;
+    }
+
+    // Inner class Node representing the Node object inside the list which have value and pointer to the next Node
+    class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
 
     public Node getHead() {
         return head;
@@ -16,6 +38,7 @@ public class LinkedList {
         return length;
     }
 
+    // Go through all the list and print each Node value
     public void printList() {
         Node temp = head;
         while (temp != null) {
@@ -41,4 +64,66 @@ public class LinkedList {
         }
     }
 
+
+    public void makeEmpty() {
+        head = null;
+        tail = null;
+        length = 0;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    public Node removeLast() {
+        if (length == 0)
+            return null;
+        Node temp = head;
+        Node pre = head;
+        while (temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        tail.next = null;
+        length--;
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+        return temp;
+    }
+
+    public void prepend (int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        length++;
+    }
+
+    public Node removeFirst(){
+        if (length == 0)
+            return null;
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        length--;
+        if (length == 0) {
+            tail = null;
+        }
+        return  temp;
+    }
 }
--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -1,4 +1,114 @@
 package LinkedList;
 
 public class Main {
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList(4);
+
+        //////////////////////////////////////////////////
+        // test printAll
+        myLinkedList.makeEmpty();
+        myLinkedList.append(1);
+        myLinkedList.append(2);
+
+        myLinkedList.printAll();
+
+        //////////////////////////////////////////////////
+        //test append
+        myLinkedList.makeEmpty();
+        myLinkedList.append(1);
+        myLinkedList.append(2);
+
+        myLinkedList.printAll();
+        /*
+       		EXPECTED OUTPUT:
+        	----------------
+        	Head: 1
+        	Tail: 2
+        	Length: 2
+
+        	Linked List:
+        	1
+        	2
+     	*/
+
+        //////////////////////////////////////////////////
+        // test removeLast
+        myLinkedList.makeEmpty();
+        myLinkedList.append(1);
+        myLinkedList.append(2);
+
+        // (2) Items - Returns 2 Node
+        System.out.println(myLinkedList.removeLast().value);
+        // (1) Item - Returns 1 Node
+        System.out.println(myLinkedList.removeLast().value);
+        // (0) Items - Returns null
+        System.out.println(myLinkedList.removeLast());
+    	/*
+        	EXPECTED OUTPUT:
+        	----------------
+        	2
+        	1
+        	null
+     	*/
+
+        //////////////////////////////////////////////////
+        // test prepend
+        myLinkedList.makeEmpty();
+        myLinkedList.append(3);
+
+        System.out.println("Before prepend():");
+        System.out.println("-----------------");
+        myLinkedList.printAll();
+
+        myLinkedList.prepend(1);
+
+        System.out.println("\n\nAfter prepend():");
+        System.out.println("----------------");
+        myLinkedList.printAll();
+        /*
+            EXPECTED OUTPUT:
+
+            Before prepend():
+            -----------------
+            Head: 2
+            Tail: 3
+            Length: 2
+
+            Linked List:
+            2
+            3
+
+            After prepend():
+            ----------------
+            Head: 1
+            Tail: 3
+            Length: 3
+
+            Linked List:
+            1
+            2
+            3
+        */
+
+        //////////////////////////////////////////////////
+        // test remove first
+        myLinkedList.makeEmpty();
+        myLinkedList.append(2);
+        myLinkedList.append(1);
+
+        // (2) Items - Returns 2 Node
+        System.out.println(myLinkedList.removeFirst().value);
+        // (1) Item - Returns 1 Node
+        System.out.println(myLinkedList.removeFirst().value);
+        // (0) Items - Returns null
+        System.out.println(myLinkedList.removeFirst());
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            2
+            1
+            null
+
+        */
+    }
 }

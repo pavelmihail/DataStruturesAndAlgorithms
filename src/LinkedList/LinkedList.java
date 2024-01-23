9e9934df75e0068c21eda72bdbc6c0c6e240bdd9
Mihail-Valeriu PAVEL <mpavel@P5RO-PORT1490.local>
2024-01-23 12:19:26 +0200

    linked list finished

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -102,7 +102,7 @@ public class LinkedList {
         return temp;
     }
 
-    public void prepend (int value) {
+    public void prepend(int value) {
         Node newNode = new Node(value);
         if (length == 0) {
             head = newNode;
@@ -114,7 +114,7 @@ public class LinkedList {
         length++;
     }
 
-    public Node removeFirst(){
+    public Node removeFirst() {
         if (length == 0)
             return null;
         Node temp = head;
@@ -124,6 +124,71 @@ public class LinkedList {
         if (length == 0) {
             tail = null;
         }
-        return  temp;
+        return temp;
+    }
+
+    public Node get(int index) {
+        if (index < 0 || index >= length)
+            return null;
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public boolean set(int index, int value) {
+        Node temp = get(index);
+        if (temp != null) {
+            temp.value = value;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean insert(int index, int value) {
+        if (index < 0 || index > length) return false;
+        if (index == 0) {
+            prepend(value);
+            return true;
+        }
+        if (index == length - 1) {
+            append(value);
+            return true;
+        }
+        Node newNode = new Node(value);
+        Node temp = get(index - 1);
+        newNode.next = temp.next;
+        temp.next = newNode;
+        length++;
+        return true;
+    }
+
+    public Node remove(int index) {
+        if (index < 0 || index >= length) return null;
+        if (index == 0) return removeFirst();
+        if (index == length - 1) return removeLast();
+
+        Node prev = get(index - 1);
+        Node temp = prev.next;
+
+        prev.next = temp.next;
+        temp.next = null;
+        length--;
+        return temp;
+    }
+
+    public void reverse() {
+        Node temp = head;
+        head = tail;
+        tail = temp;
+        Node after;
+        Node before = null;
+        for (int i = 0; i < length; i++) {
+            after = temp.next;
+            temp.next = before;
+            before = temp;
+            temp = after;
+        }
     }
 }
--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
         //////////////////////////////////////////////////
         // test printAll
+        System.out.println("/////////////////////PRINT ALL////////////////////////");
+
         myLinkedList.makeEmpty();
         myLinkedList.append(1);
         myLinkedList.append(2);
@@ -33,6 +35,8 @@ public class Main {
 
         //////////////////////////////////////////////////
         // test removeLast
+        System.out.println("/////////////////////REMOVE LAST////////////////////////");
+
         myLinkedList.makeEmpty();
         myLinkedList.append(1);
         myLinkedList.append(2);
@@ -53,6 +57,8 @@ public class Main {
 
         //////////////////////////////////////////////////
         // test prepend
+        System.out.println("/////////////////////PREPEND////////////////////////");
+
         myLinkedList.makeEmpty();
         myLinkedList.append(3);
 
@@ -92,6 +98,8 @@ public class Main {
 
         //////////////////////////////////////////////////
         // test remove first
+        System.out.println("/////////////////////REMOVE FIRST////////////////////////");
+
         myLinkedList.makeEmpty();
         myLinkedList.append(2);
         myLinkedList.append(1);
@@ -108,6 +116,222 @@ public class Main {
             2
             1
             null
+        */
+
+        //////////////////////////////////////////////////
+        // test get
+        System.out.println("/////////////////////GET////////////////////////");
+
+        myLinkedList.makeEmpty();
+
+        myLinkedList.append(0);
+        myLinkedList.append(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+
+
+        System.out.println(myLinkedList.get(3).value);
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            3
+
+        */
+
+
+        //////////////////////////////////////////////////
+        // test set
+        System.out.println("/////////////////////SET////////////////////////");
+
+        myLinkedList.makeEmpty();
+
+        myLinkedList.append(0);
+        myLinkedList.append(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+
+        System.out.println("Linked List before set():");
+        myLinkedList.printList();
+
+        myLinkedList.set(2, 99);
+
+        System.out.println("\nLinked List after set():");
+        myLinkedList.printList();
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Linked List before set():
+            0
+            1
+            2
+            3
+
+            Linked List after set():
+            0
+            1
+            99
+            3
+
+        */
+
+
+
+        //////////////////////////////////////////////////
+        // test insert
+        System.out.println("/////////////////////INSERT////////////////////////");
+
+        myLinkedList.makeEmpty();
+
+        myLinkedList.append(0);
+
+        myLinkedList.append(3);
+
+        System.out.println("LL before insert():");
+        myLinkedList.printList();
+
+        myLinkedList.insert(1, 2);
+
+        System.out.println("\nLL after insert(2) in middle:");
+        myLinkedList.printList();
+
+        myLinkedList.insert(0, 0);
+
+        System.out.println("\nLL after insert(0) at beginning:");
+        myLinkedList.printList();
+
+        myLinkedList.insert(4, 4);
+
+        System.out.println("\nLL after insert(4) at end:");
+        myLinkedList.printList();
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            LL before insert():
+            1
+            3
+
+            LL after insert(2) in middle:
+            1
+            2
+            3
+
+            LL after insert(0) at beginning:
+            0
+            1
+            2
+            3
+
+            LL after insert(4) at end:
+            0
+            1
+            2
+            3
+            4
+
+        */
+
+
+        //////////////////////////////////////////////////
+        // test remove
+        System.out.println("/////////////////////REMOVE////////////////////////");
+
+        myLinkedList.makeEmpty();
+        myLinkedList.append(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+        myLinkedList.append(4);
+        myLinkedList.append(5);
+
+        System.out.println("LL before remove():");
+        myLinkedList.printList();
+
+        System.out.println("\nRemoved node:");
+        System.out.println(myLinkedList.remove(2).value);
+        System.out.println("LL after remove() in middle:");
+        myLinkedList.printList();
+
+        System.out.println("\nRemoved node:");
+        System.out.println(myLinkedList.remove(0).value);
+        System.out.println("LL after remove() of first node:");
+        myLinkedList.printList();
+
+        System.out.println("\nRemoved node:");
+        System.out.println(myLinkedList.remove(2).value);
+        System.out.println("LL after remove() of last node:");
+        myLinkedList.printList();
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            LL before remove():
+            1
+            2
+            3
+            4
+            5
+
+            Removed node:
+            3
+            LL after remove() in middle:
+            1
+            2
+            4
+            5
+
+            Removed node:
+            1
+            LL after remove() of first node:
+            2
+            4
+            5
+
+            Removed node:
+            5
+            LL after remove() of last node:
+            2
+            4
+
+        */
+
+        //////////////////////////////////////////////////
+        // test reverse
+        System.out.println("/////////////////////REVERSE////////////////////////");
+        myLinkedList.makeEmpty();
+        myLinkedList.append(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+        myLinkedList.append(4);
+
+        System.out.println("LL before reverse():");
+        myLinkedList.printList();
+
+        myLinkedList.reverse();
+
+        System.out.println("\nLL after reverse():");
+        myLinkedList.printList();
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            LL before reverse():
+            1
+            2
+            3
+            4
+
+            LL after reverse():
+            4
+            3
+            2
+            1
 
         */
     }
